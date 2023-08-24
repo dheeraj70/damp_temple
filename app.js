28840af9f42749f6b9dd592d10b97203d5329f42
dheeraj70 <82049684+dheeraj70@users.noreply.github.com>
2023-08-24 18:52:57 +0530

    Update app.js
    Added env port

--- app.js
+++ app.js
@@ -66,6 +66,6 @@ app.get("/about",function(req,res){
   res.render("about");
 });
 
-app.listen(3000,function(req,res){
+app.listen(process.env.PORT || 3000,function(req,res){
   console.log("server started");
 });
